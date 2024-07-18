34da5c38b6d4210fa6930443de2c517546f25d81
msvalandro <msvalandro@proton.me>
2024-07-18 10:13:21 +0100

    feat: finish players list screen

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
+import { Button } from '@components/Button'
 import { ButtonIcon } from '@components/ButtonIcon'
 import { Filter } from '@components/Filter'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
+import { ListEmpty } from '@components/ListEmpty'
 import { PlayerCard } from '@components/PlayerCard'
 import { useState } from 'react'
 import { FlatList } from 'react-native'
@@ -11,7 +13,16 @@ import { Form, HeaderList, NumberOfPlayers, PlayersContainer } from './styles'
 
 export function Players() {
   const [team, setTeam] = useState('Time A')
-  const [players, setPlayers] = useState(['Harry Potter', 'Ron Weasley'])
+  const [players, setPlayers] = useState([
+    'Harry Potter',
+    'Ron Weasley',
+    'Hermione Granger',
+    'Neville Longbottom',
+    'Luna Lovegood',
+    'Ginny Weasley',
+    'Cho Chang',
+    'Draco Malfoy',
+  ])
 
   return (
     <PlayersContainer>
@@ -51,7 +62,17 @@ export function Players() {
         renderItem={({ item }) => (
           <PlayerCard name={item} onRemove={() => {}} />
         )}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não há pessoas nesse time." />
+        )}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 },
+        ]}
       />
+
+      <Button title="Remover Turma" type="SECONDARY" />
     </PlayersContainer>
   )
 }
