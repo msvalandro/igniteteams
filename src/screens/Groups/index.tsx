dab9e58bd1cc4c47625609ad7a344b0238813ae3
msvalandro <msvalandro@proton.me>
2024-07-18 20:50:02 +0100

    feat: read/save groups from storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,20 +3,36 @@ import { GroupCard } from '@components/GroupCard'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { ListEmpty } from '@components/ListEmpty'
-import { useNavigation } from '@react-navigation/native'
-import { useState } from 'react'
+import { useFocusEffect, useNavigation } from '@react-navigation/native'
+import { groupsGetAll } from '@storage/group/groupsGetAll'
+import { useCallback, useEffect, useState } from 'react'
 import { FlatList } from 'react-native'
 
 import { GroupsContainer } from './styles'
 
 export function Groups() {
-  const [groups, setGroups] = useState([])
+  const [groups, setGroups] = useState<string[]>([])
 
   const navigation = useNavigation()
 
   function handleNewGroup() {
     navigation.navigate('new')
   }
+
+  async function fetchGroups() {
+    try {
+      const data = await groupsGetAll()
+      setGroups(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups()
+    }, []),
+  )
 
   return (
     <GroupsContainer>
